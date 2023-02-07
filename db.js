8a40c03ffff85e12aaf0bbba587e9b0cfb910fde
Vasilievkosta <vasilievkosta@gmail.com>
2023-02-07 11:20:17 +0200

    changed database url

--- db.js
+++ db.js
@@ -8,15 +8,23 @@ require('dotenv').config();
     // database: process.env.DB_NAME,
     // port: process.env.DB_PORT,
 // };
+// const proConfig = {
+    // user: postgres,
+    // password: CL5mhyl8kmYz451Pu6vS,
+    // host: containers-us-west-143.railway.app,
+    // database: railway,
+    // port: 7608,
+// };
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const proConfig = `postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway`;
+// const proConfig = `postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway`;
 
-// const proConfig = process.env.DATABASE_URL //heroku addons
+
+const proConfig = process.env.DATABASE_URL //heroku addons
 
 // postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway
-
+// DATABASE_URL: postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
 const pool = new Pool({
 	
     connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,	
