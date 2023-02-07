32f9da5768e3ab4fb577bd97308c570193189efb
Vasilievkosta <vasilievkosta@gmail.com>
2023-02-07 11:47:47 +0200

    modified db

--- db.js
+++ db.js
@@ -18,10 +18,10 @@ require('dotenv').config();
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-// const proConfig = `postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway`;
+const proConfig = `postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway`;
 
 
-const proConfig = process.env.DATABASE_URL //heroku addons
+ // const proConfig = process.env.DATABASE_URL //heroku addons
 
 // postgresql://postgres:CL5mhyl8kmYz451Pu6vS@containers-us-west-143.railway.app:7608/railway
 // DATABASE_URL: postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
